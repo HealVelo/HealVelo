0d3c9ebdf45f59b73a5c7e5fa221119d02bb5260
Isnan kusuma <119983724+Isnannkusuma@users.noreply.github.com>
2026-08-29 08:26:47 +0700

    Remove comment in AppLayout.jsx
    Removed comment about background gradient blobs.

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -6,7 +6,7 @@ export default function AppLayout({ title, children }) {
     <div className="relative flex min-h-screen bg-slate-50">
       <Head title={title} />
 
-      {/* Background gradient blobs - biar efek kaca kelihatan */}
+      
       <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
         <div className="absolute -top-32 -left-20 h-96 w-96 rounded-full bg-violet-300/40 blur-3xl" />
         <div className="absolute top-1/3 -right-32 h-112 w-md rounded-full bg-fuchsia-300/30 blur-3xl" />
